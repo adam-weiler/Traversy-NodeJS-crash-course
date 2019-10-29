dc7d4097a7f1ac52ac0273fb16851ab8103ce126
adam-weiler <adam.weiler@yahoo.ca>
2019-10-29 14:49:15 -0400

    Replace person object with Person class.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const person = require('./person');
+// const person = require('./person');  // Object
+// console.log(person);
 
-console.log('Hello World!');
-console.log(person);
+
+const Person = require('./person');  // Class
+const person1 = new Person('John Doe', 30);
+
+person1.greeting();
--- person.js
+++ person.js
@@ -1,6 +1,24 @@
-const person = {
-    name: 'John Doe',
-    age: 30
+// An object:
+// const person = {
+//     name: 'John Doe',
+//     age: 30
+// }
+
+// module.exports = person;
+
+
+
+// A class:
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}.`);
+    }
 }
 
-module.exports = person;
+module.exports = Person;
